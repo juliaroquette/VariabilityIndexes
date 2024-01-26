d6c8cadabd993a40b22761304a54cacc557fae7f
Julia Roquette <juliaroquette@gmail.com>
2024-01-26 14:02:21 +0100

    sketch a class for the actual variability indexes

---
+++ variability/indexes.py
@@ -0,0 +1,18 @@
+"""
+Variability indexes
+"""
+
+class VariabilityIndex(lightcurve.LightCurve):
+    def __init__(self):
+        super().__init__()
+        
+    def M_index(self):
+        pass
+    
+    def Q_index(self):
+        pass
+    
+    def Abbe(self):
+        pass
+    
+    
