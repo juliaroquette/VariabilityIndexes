08941380f5184fe014700ced5b263d34fa84455b
Julia Roquette <juliaroquette@gmail.com>
2024-01-26 14:01:44 +0100

    sketch a filtering class

---
+++ variability/filtering.py
@@ -0,0 +1,14 @@
+"""
+Colection of fitlers
+"""
+
+import numpy as np
+
+class Filtering(lightcurve.LightCurve):
+    def __init__(self):
+        super().__init__()
+    
+    def detrend():
+        pass
+
+    def get_waveform():
