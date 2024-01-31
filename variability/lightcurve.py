f08b5b62510723c3f0506e224c90f879bba43755
Julia Roquette <juliaroquette@gmail.com>
2024-01-31 14:33:43 +0100

    Update LightCurve class

--- variability/lightcurve.py
+++ variability/lightcurve.py
@@ -1,66 +1,124 @@
 """
 Base class for reading light curves
+
+@juliaroquette:
+Defines a base class for reading light curves, which can be inherited by other classes that analyse it. 
+
+TO DO:
+Define a multi-wavelength light curve class
 """
 
 import numpy as np
-import pandas as pd
-import scipy as sp
-from astropy.timeseries import LombScargle
 
 class LightCurve:
-    
+    """
+    Class representing a light curve.
+
+    Attributes:
+        time (ndarray): Array of time values.
+        mag (ndarray): Array of magnitude values.
+        err (ndarray): Array of magnitude error values.
+    """
+
     def __init__(self, time, mag, err, mask=None):
-        self.time = np.asarray(time, dtype=float)
-        self.mag = np.asarray(mag, dtype=float)
-        self.err = np.asarray(err, dtype=float)
+        """
+        Initializes a LightCurve object.
+
+        Args:
+            time (ndarray): Array of time values.
+            mag (ndarray): Array of magnitude values.
+            err (ndarray): Array of error values.
+            mask (ndarray, optional): Array of boolean values indicating which data points to include. Defaults to None.
+        """
         if bool(mask):
-            self.mask = np.asarray(mask, dtype=bool)
+            mask = np.asarray(mask, dtype=bool)
         else:
-            self.mask = np.where(np.all(np.isfinite([mag, time, err]), axis=0))[0]
-        self.time = time[self.mask]
-        self.mag = mag[self.mask]
-        self.err = err[self.mask]
-        # if isinstance(period, float):
-            # self.period = period        
-        # elif isinstance(period, bool):
-            # if bool(period):
-                # print("Estimating a period from the LSP")
-                # f, p = self.get_simple_period()
-                # self.period = 1./f
-            # else:
-                # self.period = np.nan
-        # else:
-            # self.period = np.nan
+            mask = np.where(np.all(np.isfinite([mag, time, err]), axis=0))[0]
+        self.time = np.asarray(time, dtype=float)[mask]
+        self.mag = np.asarray(mag, dtype=float)[mask]
+        self.err = np.asarray(err, dtype=float)[mask]
         
     def N(self):
         """
-        returns the number of datapoints in the light curve
+        Returns the number of datapoints in the light curve.
+
+        Returns:
+            int: Number of datapoints.
         """
         return len(self.mag)
+    
+    def time_span(self):
+        """
+        Returns the total time-span of the light-curve
 
+        Returns:
+            float: light-curve time-span.
+        """
+        return np.max(self.time) - np.min(self.time)
+    
     def std(self):
         """
-        from numpy
+        Returns the standard deviation of the magnitude values.
+
+        Returns:
+            float: Standard deviation.
         """
         return np.std(self.mag)
 
     def mean(self):
         """
-        form numpy
+        Returns the mean of the magnitude values.
+
+        Returns:
+            float: Mean value.
         """
         return np.mean(self.mag)
 
+
     def weighted_average(self):
+        """
+        Returns the weighted average of the magnitude values.
+
+        Returns:
+            float: Weighted average.
+        """
         return np.average(self.mag, weights=1./(self.err**2))
 
     def median(self):
+        """
+        Returns the median of the magnitude values.
+
+        Returns:
+            float: Median value.
+        """
         return np.median(self.mag)
+      
 
-    def andersonDarling(self):
-        return sp.stats.anderson(self.mag)[0]
+class FoldedLightCurve(LightCurve):
+    """
+    Represents a folded light curve with time values folded for a given timescale.
 
-    def skewness(self):
-        return sp.stats.skew(self.mag, nan_policy='omit')
+    Args:
+        time (array-like): The time values of the light curve.
+        mag (array-like): The magnitude values of the light curve.
+        err (array-like): The error values of the light curve.
+        timescale (float): The timescale used for folding the light curve.
+        mask (array-like, optional): The mask to apply to the light curve.
 
-    def kurtosis(self):
-        return sp.stats.kurtosis(self.mag)
+    Attributes:
+        timescale (float): The timescale used for folding the light curve.
+        phase (array-like): The phase values of the folded light curve.
+        mag_pahsed (array-like): The magnitude values of the folded light curve, sorted based on phase.
+        err_pahsed (array-like): The error values of the folded light curve, sorted based on phase.
+    """
+
+    def __init__(self, time, mag, err, timescale, mask=None):
+        super().__init__(time, mag, err, mask=mask)
+        self.timescale = timescale 
+        # Calculate the phase values
+        phase = np.mod(self.time, self.timescale) / self.timescale
+        # Sort the phase and magnitude arrays based on phase values
+        sort = np.argsort(phase)
+        self.phase = phase[sort]
+        self.mag_pahsed = self.mag[sort]       
+        self.err_pahsed = self.err[sort]
