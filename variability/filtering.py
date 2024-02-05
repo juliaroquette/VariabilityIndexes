09534a2d3d1546d1e7e8971add4ddcd04dab8ea2
Julia Roquette <juliaroquette@gmail.com>
2024-02-05 16:15:03 +0100

    Add general detrending for uneven data

--- variability/filtering.py
+++ variability/filtering.py
@@ -14,9 +14,7 @@ class Filtering:
         self.lc = lc
         self.even = is_evenly_spaced(self)
         if not self.even:
-        warnings.warn("Time series may not be evenly spaced.", UserWarning)
-
-
+            warnings.warn("Time series may not be evenly spaced.", UserWarning)
 
     def is_evenly_spaced(self, tolerance=0.1):
         """
@@ -42,6 +40,8 @@ class Filtering:
         function conts how many points you need 
         to cover a given timescale
         """
+        if not self.even:
+            warnings.warn("Time series may not be evenly spaced - no direct conversion between number of points and timescale.", UserWarning)
         time = time - np.nanmin(time)
         return len(time[time < timescale])
     
@@ -56,33 +56,136 @@ class Filtering:
         return scipy.ndimage.median_filter(self.lc.mag, size=window, mode='nearest')
     
         
-    def savgol_longtrend(time, mag) :
+    def savgol_longtrend(self, timescale=10., polynom=3) :
         """
         To be changed to timescale rather than window size
         """
-        ws =  int(0.25*len(mag))
-        return savgol_filter(mag, ws, 3)
+        if not self.even:
+            warnings.warn("SavGol Scipy expects even time series - which may not be true here.", UserWarning)        
+        window = get_num_time(self.lc.time, timescale)
+        return savgol_filter(self.lc.mag, window, polynom)
     
+    def uneven_savgol(self, window, polynom):
+        """
+        Applies a Savitzky-Golay filter to y with non-uniform spacing
+        as defined in x
 
+        This is based on https://dsp.stackexchange.com/questions/1676/savitzky-golay-smoothing-filter-for-not-equally-spaced-data
+        The borders are interpolated like scipy.signal.savgol_filter would do
+
+        Parameters
+        ----------
+        window : int (odd)
+            Window length of datapoints. Must be odd and smaller than x
+        polynom : int
+            The order of polynom used. Must be smaller than the window size
+
+        Returns
+        -------
+        np.array of float
+            The smoothed y values
+        """
+        x = self.lc.time
+        y = self.lc.mag
+
+        if len(x) < window:
+            raise ValueError('The data size must be larger than the window size')
+
+        if type(window) is not int:
+            raise TypeError('"window" must be an integer')
+
+        if window % 2 == 0:
+            raise ValueError('The "window" must be an odd integer')
+
+        if type(polynom) is not int:
+            raise TypeError('"polynom" must be an integer')
+
+        if polynom >= window:
+            raise ValueError('"polynom" must be less than "window"')
+
+        half_window = window // 2
+        polynom += 1
+
+        # Initialize variables
+        A = np.empty((window, polynom))     # Matrix
+        tA = np.empty((polynom, window))    # Transposed matrix
+        t = np.empty(window)                # Local x variables
+        y_smoothed = np.full(len(y), np.nan)
+
+        # Start smoothing
+        for i in range(half_window, len(x) - half_window, 1):
+            # Center a window of x values on x[i]
+            for j in range(0, window, 1):
+                t[j] = x[i + j - half_window] - x[i]
+
+            # Create the initial matrix A and its transposed form tA
+            for j in range(0, window, 1):
+                r = 1.0
+                for k in range(0, polynom, 1):
+                    A[j, k] = r
+                    tA[k, j] = r
+                    r *= t[j]
+
+            # Multiply the two matrices
+            tAA = np.matmul(tA, A)
+
+            # Invert the product of the matrices
+            tAA = np.linalg.inv(tAA)
+
+            # Calculate the pseudoinverse of the design matrix
+            coeffs = np.matmul(tAA, tA)
+
+            # Calculate c0 which is also the y value for y[i]
+            y_smoothed[i] = 0
+            for j in range(0, window, 1):
+                y_smoothed[i] += coeffs[0, j] * y[i + j - half_window]
+
+            # If at the end or beginning, store all coefficients for the polynom
+            if i == half_window:
+                first_coeffs = np.zeros(polynom)
+                for j in range(0, window, 1):
+                    for k in range(polynom):
+                        first_coeffs[k] += coeffs[k, j] * y[j]
+            elif i == len(x) - half_window - 1:
+                last_coeffs = np.zeros(polynom)
+                for j in range(0, window, 1):
+                    for k in range(polynom):
+                        last_coeffs[k] += coeffs[k, j] * y[len(y) - window + j]
+
+        # Interpolate the result at the left border
+        for i in range(0, half_window, 1):
+            y_smoothed[i] = 0
+            x_i = 1
+            for j in range(0, polynom, 1):
+                y_smoothed[i] += first_coeffs[j] * x_i
+                x_i *= x[i] - x[half_window]
+
+        # Interpolate the result at the right border
+        for i in range(len(x) - half_window, len(x), 1):
+            y_smoothed[i] = 0
+            x_i = 1
+            for j in range(0, polynom, 1):
+                y_smoothed[i] += last_coeffs[j] * x_i
+                x_i *= x[i] - x[-half_window - 1]
+
+        return y_smoothed
 
    
-
     
+    def smooth_per_timescale(self, window_days=10.):
+        """
+        for given light-curve with a time and mag, 
+        use a window wd in days to smooth the light-curve
+        then remove the smoothed curve form the 
+        """
+        smoothed_values = np.zeros_like(self.lc.mag, dtype=float)
+        for i in range(len(self.lc.time)):
+            start_time = self.lc.time[i] - window_days/2
+            end_time = self.lc.time[i] + window_days/2
+            select = np.where((self.lc.time >= start_time) & (self.lc.time <= end_time))
+            smoothed_values[i] = np.mean(self.lc.mag[select])
+        return smoothed_values
     
-    # def smooth(self, window_days=20.):
-    
-    #     for given light-curve with a time and mag, 
-    #     use a window wd in days to smooth the light-curve
-    #     then remove the smoothed curve form the 
-    #     """
-    #     pass
-        # smoothed_values = np.zeros_like(self.lc.mag, dtype=float)
-        # for i in range(len(self.lc.time)):
-        #     start_time = self.lc.time[i] - window_days/2
-        #     end_time = self.lc.time[i] + window_days/2
-        #     select = np.where((self.lc.time >= start_time) & (self.lc.time <= end_time))
-        #     smoothed_values[i] = np.nanmean(self.lc.mag[select])
-        # self.detrended_mag = self.lc.mag - smoothed_values + np.nanmean(self.lc.mag)
     
 
 
