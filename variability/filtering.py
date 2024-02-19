f737af6870df070f8ed0b291b9e2b6c92df736c3
Julia Roquette <juliaroquette@gmail.com>
2024-02-19 16:34:04 +0100

    improved implementation for filtering

--- variability/filtering.py
+++ variability/filtering.py
@@ -141,28 +141,34 @@ class Filtering:
 
 
 class WaveForm:
-    def __init__(self, folded_lc, method=''):
-        # self.phase, self.mag_pahsed, self.err_pahsed
+    def __init__(self, folded_lc, waveform_type='uneven_savgol'):
+        """
+        Initialize the WaveForm class.
+
+        Parameters:
+        - folded_lc (FoldedLightCurve): The folded light curve to be analyzed.
+        - method (str): The waveform analysis method to be used.
+
+        Raises:
+        - TypeError: If folded_lc is not an instance of FoldedLightCurve.
+        """
         if not isinstance(folded_lc, FoldedLightCurve):
             raise TypeError("lc must be an instance of LightCurve")
         else:
             self._lc = folded_lc
+        self._waveform_type = waveform_type
   
     def circular_rolling_average_number(self, window_size=5):
         """
-        Calculates the rolling average while centering the data around the phase value.
-        
+        Calculate the circular rolling average of the folded light curve.
+
         Parameters:
-            mag_phased (array-like): Array of phased data.
-            window_size (int): Size of the rolling window in number of datapoints. Default is 5.
-            
+        - window_size (int): The window size for the rolling average.
+
         Returns:
-            waveform (array-like): Array with the waveform .
-            
-        Assumes phased data is circular.
-        """        
-        extended_mag = np.concatenate((self._lc.mag_phased, self._lc.mag_phased,
-                                       self._lc.mag_phased))
+        - np.array: The circular rolling average waveform.
+        """
+        extended_mag = np.concatenate((self._lc.mag_phased, self._lc.mag_phased, self._lc.mag_phased))
         extended_waveform = np.array([
             np.mean(extended_mag[i - int(0.5 * window_size):
                 i + int(np.round((0.5 * window_size)))])\
@@ -179,20 +185,18 @@ class WaveForm:
 
     def circular_rolling_average_phase(self, wd_phase=0.1):
         """
-        Calculate the waveform phase fraction.
+        Calculate the circular rolling average of the folded light curve based on phase.
 
         Parameters:
-        - phase (array-like): Array of phase values.
-        - mag_phased (array-like): Array of magnitude values.
-        - wd_phase (float, optional): Width of the phase window in % of phase. Default is 0.1.
+        - wd_phase (float): The phase window size for the rolling average.
 
         Returns:
-        - waveform (array-like): Array with the waveform .
+        - np.array: The circular rolling average waveform based on phase.
         """
         waveform = np.full(len(self._lc.phase), np.nan)
         extended_phase = np.concatenate((self._lc.phase - 1, self._lc.phase, 1 + self._lc.phase))
         extended_mag = np.concatenate((self._lc.mag_phased, self._lc.mag_phased, self._lc.mag_phased))
-        for i, p in enumerate(self._lc.phase[3:]):
+        for i, p in enumerate(self._lc.phase):
             select = np.where((extended_phase <= p + wd_phase/2.) & (extended_phase > p - wd_phase/2.))[0]
             waveform[i] = np.mean(extended_mag[select])
         return waveform
@@ -214,30 +218,53 @@ class WaveForm:
         return sp.ndimage.filters.median_filter(self._lc.mag_phased, size=n_point, mode='wrap')   
     
     def uneven_savgol(self, window, polynom):
-        x = self._lc.phase
-        y = self._lc.mag_phased
-        return uneven_savgol(x, y, window, polynom)     
+        """
+        Apply a Savitzky-Golay filter to the folded light curve with non-uniform spacing.
+
+        Parameters:
+        - window (int): Window length of datapoints. Must be odd and smaller than the light curve size.
+        - polynom (int): The order of polynomial used. Must be smaller than the window size.
+
+        Returns:
+        - np.array: The smoothed folded light curve.
+
+        Raises:
+        - ValueError: If the data size is smaller than the window size or if the window is not an odd integer.
+        - TypeError: If window or polynom are not integers.
+        """
+        x = np.concatenate((self._lc.phase - 1, self._lc.phase, 1 + self._lc.phase))
+        y = np.concatenate((self._lc.mag_phased, self._lc.mag_phased, self._lc.mag_phased)) 
+        return  uneven_savgol(x, y, window, polynom)  [self._lc.N:2*self._lc.N]
     
-    def residual_magnitude(self, waveform_type='savgol', **kargs):
+    def residual_magnitude(self, waveform_type='uneven_savgol', **kwargs):
         """
         Calculate the residual magnitude after waveform subtraction.
         """
         if waveform_type == 'savgol':
-            waveform = self.savgol(**kargs)
-        elif waveform_type == 'circular_rolling_average_number':
-            waveform = self.circular_rolling_average_number(**kargs)
-        elif waveform_type == 'circular_rolling_average_phase':
-            waveform = self.circular_rolling_average_phase(**kargs)
-        elif waveform_type == 'H22':
-            waveform = self.waveform_H22(**kargs)
+            #needs to catch kwarfgs
+            waveform = self.savgol(**kwargs)
         elif waveform_type == 'Cody':
-            waveform = self.waveform_Cody(**kargs)
-        elif waveform_type == 'uneven_savgol':
-            waveform = self.uneven_savgol(**kargs)
+            #needs to catch kwarfgs
+            waveform = self.waveform_Cody(**kwargs)
+        elif self._waveform_type == 'circular_rolling_average_phase':
+            wd_phase = kwargs.get('wd_phase', 0.1)
+            waveform = self.circular_rolling_average_phase(wd_phase=wd_phase)
+        elif self._waveform_type == 'circular_rolling_average_number':
+            window_size = kwargs.get('window_size', 0.1*self._lc.N)
+            waveform = self.circular_rolling_average_number(window_size=window_size)
+        elif self._waveform_type == 'H22':
+            kernel = kwargs.get('kernel', 4.)
+            waveform = self.waveform_H22(kernel=kernel)
+        elif self._waveform_type == 'uneven_savgol':
+            wd = round(0.1*self._lc.N)
+            if wd % 2 == 0:
+                wd += 1
+            window = kwargs.get('window', wd)
+            polynom = kwargs.get('polynom', 3)
+            waveform = self.uneven_savgol(window, polynom)
         else:
-            raise ValueError("Method _{0}_ not implemented.".format(waveform_type))
+            raise ValueError("Method _{0}_ not implemented.".format(self._waveform_type))
         return self._lc.mag_phased - waveform
-
 
 def uneven_savgol(x, y, window, polynom):
     """
@@ -340,5 +367,77 @@ def uneven_savgol(x, y, window, polynom):
         for j in range(0, polynom, 1):
             y_smoothed[i] += last_coeffs[j] * x_i
             x_i *= x[i] - x[-half_window - 1]
+
+    return y_smoothed
+
+def uneven_savgol_(x, y, window, polynom):
+    """
+    Applies a Savitzky-Golay filter to y with non-uniform spacing
+    as defined in x
+
+    This is based on https://dsp.stackexchange.com/questions/1676/savitzky-golay-smoothing-filter-for-not-equally-spaced-data
+    The borders are interpolated like scipy.signal.savgol_filter would do
+
+    Parameters
+    ----------
+    window : int (odd)
+        Window length of datapoints. Must be odd and smaller than x
+    polynom : int
+        The order of polynom used. Must be smaller than the window size
+
+    Returns
+    -------
+    np.array of float
+        The smoothed y values
+    """
+    if len(x) != len(y):
+        raise ValueError('"x" and "y" must be of the same size')
+    if len(x) < window:
+        raise ValueError('The data size must be larger than the window size')
+
+    if type(window) is not int:
+        raise TypeError('"window" must be an integer')
+
+    if window % 2 == 0:
+        raise ValueError('The "window" must be an odd integer')
+
+    if type(polynom) is not int:
+        raise TypeError('"polynom" must be an integer')
+
+    if polynom >= window:
+        raise ValueError('"polynom" must be less than "window"')
+
+    half_window = window // 2
+    polynom += 1
+    
+    # Initialize variables
+    t = np.empty(window)                # Local x variables
+    y_smoothed = np.full(len(y), np.nan)
+
+    # Calculate powers of t
+    powers_of_t = np.empty((window, polynom))
+    for j in range(window):
+        powers_of_t[j] = np.power(x[half_window + j] - x[half_window], np.arange(polynom))
+
+    # Start smoothing
+    for i in range(half_window, len(x) - half_window):
+        # Calculate t vector
+        t = x[i - half_window:i + half_window + 1] - x[i]
+
+        # Create the initial matrix A and its transposed form tA
+        A = np.vander(t, polynom, increasing=True)
+        tA = A.T
+
+        # Multiply the two matrices
+        tAA = np.matmul(tA, A)
+
+        # Invert the product of the matrices
+        tAA_inv = np.linalg.inv(tAA)
+
+        # Calculate the pseudoinverse of the design matrix
+        coeffs = np.matmul(tAA_inv, tA)
+
+        # Calculate smoothed y value
+        y_smoothed[i] = np.dot(coeffs[0], y[i - half_window:i + half_window + 1])
 
     return y_smoothed
