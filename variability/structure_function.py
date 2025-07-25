67eb30b987c29dbb11865194aab2c9cf3157a292
Julia Roquette <juliaroquette@gmail.com>
2025-07-25 11:53:58 +0200

    Binning completed

--- variability/structure_function.py
+++ variability/structure_function.py
@@ -99,92 +99,84 @@ def get_sf_time_lags(time, mag, err):
     return time_lag[sort_idx], sf_vals[sort_idx], sf_err[sort_idx]
 
 def adaptative_binning(time_lag, sf_values, sf_err=None,
-                       num_bins=100, 
                        log=True,
                        hybrid=False,
-                       min_bin_num=5, 
-                       max_expand_factor=3.0):
+                       bin_min_size=5, 
+                       max_bin_exp_factor=3.0, 
+                       resolution=0.15):
     """
-    Suite of binning strategies for structure function data.
-    Generally, data can be binned in either log/linear space, while ensuring
-    that bins have at least min_bin_num pairs.
+    Suite of adaptative binning methods.
+    This bins data in either log/linear space, while ensuring
+    that bins have at least bin_min_size SF data points.
     Parameters:
         time_lag:    array of SF time lags
         sf_values:   array of SF values (same length as time_lag)
         sf_err:      array of SF errors (same length as time_lag)
                      If None, error is estimated from sf_values
-        num_bins:    desired number of bins (centers)
-                     Considers the number of bins in the context
-                     of uniformly spaced time lags. The real 
-                     number of populated bins will be less or 
-                     equal to num_bins.
-        scale:       'log' or 'linear'          
-        min_bin_num:   limit number of pairs per bin. 
-                    If hybrid=False: if the number of pairs in a bin 
-                    is less than min_bin_num, the bin is iteratively 
-                    expanded around its center by a factor of 1.1 until
-                    a maximum expansion factor (set by `max_expand_factor)
-                    is reached. 
-                    If hybrid=False, if the number of pairs in a bin is less
-                    than min_bin_num, the few existent SF pairs are stored in 
-                    a list for later processing.
+        scale:       'log' or 'linear'
         hybrid:      True/False
-        max_expand_factor: maximum factor by which to expand bin width
+        bin_min_size:   limit number of pairs per bin. 
+                    If hybrid=False: if the number of pairs in a bin 
+                    is less than bin_min_size, the bin is iteratively 
+                    expanded around its center in steps of 20% of the 
+                    resolution until a maximum expansion factor 
+                    (set by `max_bin_exp_factor) is reached. 
+                    If hybrid=False, if the number of pairs in a bin is less
+                    than bin_min_size, the few existent SF pairs are stored in 
+                    a list for later processing.
+        max_bin_exp_factor: maximum factor by which to expand bin width. 
+                    It is clipped to 3.0, which is the case when the bin is merged to
+                    its two immediate neighbours.
+        resolution: Resolution for binning. If log=True, this is in dex.
+                    If log=False, this is in linear units.
+                    Recommended maximum value for log scale is 0.15 for log binning 
+                    (guarantees a 5 days timescale is distinguishable from a 7 days one)
     Returns:
         sf_binned, time_bins, sf_bin_err, pair_counts, irregular_bin
         where:
         - sf_binned:   binned SF values
         - time_bins:   centers of the bins
-        - sf_bin_err:  binned SF errors 
-                       (or standard error of the mean if sf_err is None)
+        - sf_bin_err:  binned SF errors (or standard error of the mean if sf_err is None)
         - pair_counts: number of pairs in each bin
         - irregular_bin: flag indicating if the bin is irregular 
-                         1.0 if regular, 
+                         0.0 if regular, 
                          -1.0 if using individual pairs
-                         >1.0 final factor of bin expansion
-    Note on binning strategies::
-        - If `log` is True, bins are logarithmically spaced. 
-            When data is unevenly spaced, and sparse, log binning is preferred.
-        
+                         >0.0 final expanded bin width used
     """
-    tmin, tmax = np.min(time_lag), np.max(time_lag)
-    # Bin centers
-    if log:
-        bin_edges = np.logspace(np.log10(tmin), np.log10(tmax), num_bins+1)
-    else:
-        bin_edges = np.linspace(tmin, tmax, num_bins+1)
-    
+
+    # note that we need some limit in this max_bin_exp_factor
+    # otherwise the bin expansion can completely mess
+    # the resolutions of the binning
+    # the limit set here is the case when the bin is merged 
+    # with its two immediate neighbours
+    max_bin_exp_factor = min(max_bin_exp_factor, 3.)
+
+    # get bins and edges:
+    bin_edges, centres  = bin_edges_and_centers(time_lag, resolution, log=log)
+        
     sf_binned, time_bins, sf_bin_err, pair_counts, irregular_bin = [], [], [], [], []
-    bin_idxs = np.digitize(time_lag, bin_edges, right=True) - 1  # subtract 1 for 0-based bins
+    bin_idxs = np.digitize(time_lag, bin_edges, right=True) - 1  # subtract 1 for bin indexes starting at 0
     for i in range(len(bin_edges) - 1):
 
         # Get indices for current bin
         bin_idx = np.where(bin_idxs == i)[0]
+        
         # test if there is at least one pair in the bin
         # otherwise discard it 
-        
         if (len(bin_idx) > 0) :
-
-            # get the center of the bin
-            bin_left = bin_edges[i]
-            bin_right = bin_edges[i + 1]
-            if log: #geometric mean
-                centre = np.sqrt(bin_left * bin_right)
-            else:
-                centre = (bin_left + bin_right) / 2.0
-            # test if bin has enough pairs    
-            if len(bin_idx) >= min_bin_num:
-                time_bins.append(centre)
+            if len(bin_idx) >= bin_min_size:
+                time_bins.append(centres[i])
                 sf_binned.append(np.mean(sf_values[bin_idx]))
                 pair_counts.append(len(bin_idx))
-                irregular_bin.append(1.0)
+                irregular_bin.append(0.0) # no irregular flagging
                 if sf_err is not None:
                     # error propagation
                     sf_bin_err.append(np.sqrt(np.sum(sf_err[bin_idx]**2)) / (len(bin_idx)))
                 else:
                     # standard error of the mean
                     sf_bin_err.append(np.std(sf_values[bin_idx], ddof=1) / np.sqrt(len(bin_idx)))
-            elif len(bin_idx) > 0:
+            # if minimum number of pairs is not reached
+            elif len(bin_idx) > 0:                            
                 # if hybrid binning is chose, append the few datapoints
                 if bool(hybrid):
                     for j in bin_idx:
@@ -196,40 +188,98 @@ def adaptative_binning(time_lag, sf_values, sf_err=None,
                         if sf_err is not None:
                             sf_bin_err.append(sf_err[j])
                         else:
-                            # if no uncertainty is provided, use the average of the bin errors calculated so far
-                            sf_bin_err.append(np.nanmedian(sf_bin_err))
+                            # if no uncertainty is provided, use the MAD for the full dataset
+                            sf_bin_err.append(1.4826*np.median(np.abs(sf_values-np.median(sf_values))))
                 else:
                     # if hybrid binning is not chosen, attempt to
                     # expand bin until either it has enough pairs 
-                    # or it reaches the maximum expansion factor     
-                    expand = 1.0           
-                    while len(bin_idx) < min_bin_num and expand <= max_expand_factor:
-                        expand *= 1.1
-                        if log:    
-                            log_center = np.log10(centre)
-                            log_halfwidth = (np.log10(bin_right) - np.log10(bin_left)) / 2
-                            log_left = log_center - log_halfwidth * expand
-                            log_right = log_center + log_halfwidth * expand
-                            # makes sure I am not going beyong the limits
-                            # of my binning
-                            left = max(10 ** log_left, tmin)
-                            right = min(10 ** log_right, tmax)
-                            
+                    # or it reaches the max_bin_width allowed
+                    new_width = resolution
+                    step = 0.2 * resolution # use 20% of the resolution as an increment
+                    found = False
+                    while (new_width < max_bin_exp_factor * resolution):
+                        # add increment of 20% the resolution
+                        new_width += step
+                        if log:
+                            log_center = np.log10(centres[i])                       
+                            log_left = log_center - new_width / 2.0
+                            log_right = log_center + new_width / 2.0
+                            # Update the data in the bin
+                            bin_idx = np.where((time_lag > 10**log_left) & (time_lag < 10**log_right))[0]
                         else:
                             # linear expansion
-                            left = max(centre - (bin_right - bin_left) * expand / 2.0, tmin)
-                            right = min(centre + (bin_right - bin_left) * expand / 2.0, tmax)      
-                        bin_idx = np.where((time_lag >= left) & (time_lag < right))[0]
-                    if len(bin_idx) >= min_bin_num:
-                        time_bins.append(centre)
+                            left = centres[i] - new_width / 2.0
+                            right = centres[i] + new_width / 2.0
+                            bin_idx = np.where((time_lag >= left) & (time_lag < right))[0]
+                        if (len(bin_idx) >= bin_min_size):
+                            found = True
+                            break
+                    if found:
+                        time_bins.append(centres[i])
                         sf_binned.append(np.mean(sf_values[bin_idx]))
                         pair_counts.append(len(bin_idx))
-                        irregular_bin.append(expand)
+                        irregular_bin.append(new_width)
                         if sf_err is not None:
                             sf_bin_err.append(np.sqrt(np.sum(sf_err[bin_idx]**2)) /len(bin_idx))
                         else:
-                            sf_bin_err.append(np.std(sf_values[bin_idx], ddof=1) / np.sqrt(len(bin_idx)))                  
+                            sf_bin_err.append(np.std(sf_values[bin_idx], ddof=1) / np.sqrt(len(bin_idx)))
     return np.array(sf_binned), np.array(time_bins), np.array(sf_bin_err), np.array(pair_counts), np.array(irregular_bin)
+
+
+def bin_edges_and_centers(time_lag, 
+                        resolution,
+                        log=True):
+    """Given an array of time lags and the 
+    choice of a binning resolution, returns the edges 
+    and centres of the bins in either linear or log space.
+    Assumes that min(time_lag) and max(time_lag) should be
+    at the bin centres.
+
+    Args:
+        time_lag: Structure Function time lags
+        resolution: resolution for binning.
+                    If log=True, this is in dex, 
+                    where N = 1 + int((log10(max(time_lag))-log10(min(time_lag)))/resolution)
+                    If log=False, this is in days, 
+                    where N = 1 + int((max(time_lag)-min(time_lag))/resolution)
+        log: True for binning in log space,
+             False for binning in linear space.
+
+    Returns:
+        edges: array of bin edges
+        centres: array of bin centres
+        Note that len(edges) == len(centres) + 1
+    """
+    tmin = np.min(time_lag)
+    tmax = np.max(time_lag)
+    if log:
+        log_tmin = np.log10(tmin)
+        log_tmax = np.log10(tmax)
+        # decides number of bins based on the resolution in dex
+        n_bins = 1 + int(round((log_tmax - log_tmin) / resolution))
+        # makes sure the edge bins are centred at max/min values
+        centres_log = np.linspace(log_tmin, log_tmax, n_bins)
+        centres = 10**centres_log
+        # set the edges given the resolution and the correct bin centre
+        edges = np.empty(n_bins + 1)
+        # first edge
+        edges[0] = 10**(centres_log[0] - resolution/2)
+        # last edge
+        edges[-1] = 10**(centres_log[-1] + resolution/2)
+        # intermediate edges
+        for i in range(1, n_bins):
+            edges[i] = 10**((centres_log[i-1] + centres_log[i])/2)
+    else:
+        # same here but in linear space
+        n_bins = 1 + int(round((tmax - tmin) / resolution))
+        centres = np.linspace(tmin, tmax, n_bins)
+        edges = np.empty(n_bins + 1)
+        edges[0] = centres[0] - resolution/2
+        edges[-1] = centres[-1] + resolution/2
+        for i in range(1, n_bins):
+            edges[i] = (centres[i-1] + centres[i]) / 2
+    # len(edges) == len(centres) + 1
+    return edges, centres         
 
 def model_function(x, t0, C0, C1, beta):
     """
